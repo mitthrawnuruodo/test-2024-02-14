c2427894e6589e54656eb75b95c18724d54e05b7
Lasse Hægland <seilenos@gmail.com>
2024-02-15 07:34:26 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -10,7 +10,7 @@ document.addEventListener("click", (e) => {
 });
 
 const routes = {
-    "404": {
+    404: {
         template: "/templates/404.html",
         title: "404",
         description: "Page not found",
